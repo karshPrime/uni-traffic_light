9381b80b2388bd2006d3fa2b8fa8d262f713ebd1
ut-kr <utkarsh@cyberfear.com>
2022-05-20 22:40:34 +1000

    ✨ implemented basic loop

--- TopLevel.vhdl
+++ TopLevel.vhdl
@@ -7,48 +7,91 @@
 -- Controls two sets of lights consisting of Red, Amber and Green traffic lights and
 -- a pedestrian walk light.
 ----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity Traffic is
-    Port ( Reset      : in   STD_LOGIC;
-           Clock      : in   STD_LOGIC;
-           
-           -- for debug
-           debugLED   : out  std_logic;
-           LEDs       : out  std_logic_vector(2 downto 0);
+    Port (	Reset      : in   STD_LOGIC;
+				Clock      : in   STD_LOGIC;
 
-           -- Car and pedestrian buttons
-           CarEW      : in   STD_LOGIC; -- Car on EW road
-           CarNS      : in   STD_LOGIC; -- Car on NS road
-           PedEW      : in   STD_LOGIC; -- Pedestrian moving EW (crossing NS road)
-           PedNS      : in   STD_LOGIC; -- Pedestrian moving NS (crossing EW road)
-           
+				-- for debug
+				debugLED   : out  std_logic;
+				LEDs       : out  std_logic_vector(2 downto 0);
+
+				-- Car and pedestrian buttons
+				CarEW      : in   STD_LOGIC; -- Car on EW road
+				CarNS      : in   STD_LOGIC; -- Car on NS road
+				PedEW      : in   STD_LOGIC; -- Pedestrian moving EW (crossing NS road)
+				PedNS      : in   STD_LOGIC; -- Pedestrian moving NS (crossing EW road)
+
            -- Light control
-           LightsEW   : out STD_LOGIC_VECTOR (1 downto 0); -- controls EW lights
-           LightsNS   : out STD_LOGIC_VECTOR (1 downto 0)  -- controls NS lights
+				LightsEW   : out STD_LOGIC_VECTOR (1 downto 0); -- controls EW lights
+				LightsNS   : out STD_LOGIC_VECTOR (1 downto 0)  -- controls NS lights
            
            );
 end Traffic;
+----------------------------------------------------------------------------------
 
 architecture Behavioral of Traffic is
-
 -- Encoding for lights
 constant RED   : std_logic_vector(1 downto 0) := "00";
 constant AMBER : std_logic_vector(1 downto 0) := "01";
 constant GREEN : std_logic_vector(1 downto 0) := "10";
 constant WALK  : std_logic_vector(1 downto 0) := "11";
 
+-- defining states
+type StateType is (GreenNS, GreenEW, AmberEW, AmberNS);
+signal State, NextState : StateType;
+
 begin
-   -- Show reset status on FPGA LED
-   debugLed <= Reset; 
-   
-   -- Threee LEDs for debug 
-   LEDs     <= "000";
-   
-   -- Luck North-south traffic
-   LightsEW <= RED;
-   LightsNS <= WALK;
+	debugLed <= Reset; 		-- Show reset status on FPGA LED
+	LEDs     <= "000";		-- Threee LEDs for debug 
+
+
+	-- Chance state on clock edge, and reset state if requested
+	SyncProcess:
+	Process(Reset, Clock)
+	begin
+		if (Reset = '1') then
+			State <= GreenEW;
+		elsif rising_edge(clock) then
+			State <= NextState;
+		end if;
+	end Process SyncProcess;
+
+
+	-- 
+	CombinationalProcess:
+	Process(State)
+	begin
+		-- default values for signals; prevents latches
+		LightsEW  <= RED;
+		LightsNS  <= RED;
+		NextState <= State;
+
+		-- Next state and lights condition defined based upon current state
+		case State is
+			when GreenNS =>
+				LightsNS  <= GREEN;
+				NextState <= AmberNS;
+				
+			when AmberNS =>
+				LightsNS  <= AMBER;
+				NextState <= GreenEW;
+				
+			when GreenEW =>
+				LightsNS  <= RED;
+				LightsEW  <= GREEN;
+				NextState <= AmberEW;
+				
+			when AmberEW =>
+				LightsEW  <= AMBER;
+				NextState <= GreenNS;
+		end case;
+
+	end Process CombinationalProcess;
+
 end;
